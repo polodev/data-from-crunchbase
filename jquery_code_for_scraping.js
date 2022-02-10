2454f1e43967ec5979eadc1f83fec9976a3d778a
polo <polodev10@gmail.com>
2022-02-10 15:12:14 +0600

    updated script

--- jquery_code_for_scraping.js
+++ jquery_code_for_scraping.js
@@ -1,3 +1,32 @@
+
+//1. for adding jquery
+var s = document.createElement('script');
+s.src = "https://code.jquery.com/jquery-3.6.0.min.js";
+document.getElementsByTagName('head')[0].appendChild(s)
+
+// 2. scrping header row
+var grid_column_header  = [];
+$('grid-column-header').each(function () {
+    var text = $(this).text();
+    grid_column_header.push(text);
+})
+copy(grid_column_header);
+
+// 3. Scraping each row
+var data  = [];
+$('grid-row').each(function () {
+  var grid_cells_data = [];
+  $(this).find('grid-cell').each(function () {
+    var text = $(this).text();
+    grid_cells_data.push(text);
+  })
+  data.push(grid_cells_data)
+})
+copy(data);
+
+
+
+
 var grid_row = document.querySelectorAll('grid-row');
 var grid_row_array = [];
 grid_row.forEach(function (item) {
@@ -21,9 +50,6 @@ $('grid-row').each(function() {
   console.log(this);
 })
 
-var s = document.createElement('script');
-s.src = "https://code.jquery.com/jquery-3.6.0.min.js";
-document.getElementsByTagName('head')[0].appendChild(s)
 
 
 var grid_column_header  = [];
